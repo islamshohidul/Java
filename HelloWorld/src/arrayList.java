b72cfba4dfbdbe6fade2efede4fb57675759f2b3
shohidul islam <shohidul>
2018-11-20 00:11:33 +0800

    ArrayList

---
+++ HelloWorld/src/arrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class arrayList {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("California");
+		list.add("Jamalpur");
+		list.add("Dhaka");
+		System.out.println(list);
+		list.remove(0);
+		System.out.println(list);
+		
+		
+
+
+
+	}
+
+}
